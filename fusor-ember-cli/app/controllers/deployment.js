e9afa6aa0e07407b039a401a49e9a2c567d2b16d
Erik Nelson <erik@nsk.io>
2016-05-26 20:17:05 -0400

    DRY and clean up upstream consumer logic
    - Started to consolidate repeated aliases in commonly used mixin
    - Removed organizationUpstreamConsumerUUID, not in use currently
    - Removed redundant upstream consumer loading

--- fusor-ember-cli/app/controllers/deployment.js
+++ fusor-ember-cli/app/controllers/deployment.js
@@ -87,12 +87,9 @@ export default Ember.Controller.extend(DeploymentControllerMixin, DisableTabMixi
   ),
 
   hasSubscriptionUUID: Ember.computed(
-    'organizationUpstreamConsumerUUID',
     'model.upstream_consumer_uuid',
     function() {
-      return (Ember.isPresent(this.get('organizationUpstreamConsumerUUID')) ||
-              Ember.isPresent(this.get('model.upstream_consumer_uuid'))
-             );
+      return Ember.isPresent(this.get('model.upstream_consumer_uuid'));
     }
   ),
 
@@ -102,7 +99,14 @@ export default Ember.Controller.extend(DeploymentControllerMixin, DisableTabMixi
     'hasSubscriptionUUID',
     'disableNextOnSelectSubscriptions',
     function() {
-      return (!this.get('isDisconnected') && (this.get('isDisabledSubscriptions') || !this.get("hasSubscriptionUUID") || this.get('disableNextOnSelectSubscriptions')));
+      const isConnectedSync = !this.get('isDisconnected');
+      const subsNotReady =
+       this.get('isDisabledSubscriptions') ||
+       !this.get("hasSubscriptionUUID") ||
+       this.get('disableNextOnSelectSubscriptions');
+
+      // Disable review if this is a connected sync and subs are not ready
+      return isConnectedSync && subsNotReady;
     }
   ),
 
